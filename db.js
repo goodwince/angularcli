411d04bec6bf03361b4a685e58a850854768423a
goodwince <johnathan.goodwin@gmail.com>
2018-05-14 20:20:58 +0000

    Created auth api

---
+++ db.js
@@ -0,0 +1,2 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://juntolevel:m04Ovg3WSV3u@ds149934.mlab.com:49934/juntodev');
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const path = require('path');
 const express = require('express');
+var bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+
 var apiHeroRoutes = require('./routes/apiHero');
 var apiRoutes = require('./routes/api');
+var authController = require('./routes/authController');
+
+var db = require('./db');
 
 // If an incoming request uses
 // a protocol other than HTTPS,
@@ -30,8 +37,11 @@ if (env == 'production') {
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
+//routing and controllers
+
 
 app.use('/api/heroes', apiHeroRoutes);
+app.use('/api/auth', authController);
 app.use('/api', apiRoutes);
 
 //all non-api urls return dashboard
@@ -39,7 +49,16 @@ app.get('/*', function(req, res) {
    res.sendFile(path.join(__dirname + '/dist/angular-tour-of-heroes/index.html'));
 }); 
 
+app.use(function logJsonParseError(err, req, res, next) {
+  if (err.status === 400 && err.name === 'SyntaxError' && err.body) {
+    // Display extra information for JSON parses
+    console.log('JSON body parser error.')
+    console.log(req.method + ' ' + req.url)
+    console.log(err.body.slice(0, 100).toString())
+  }
 
+  next(err)
+})
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
